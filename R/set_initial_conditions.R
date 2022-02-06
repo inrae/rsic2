12ce49219378d19c1e19fe09d22f89ab57dd03cf
Dorchies David <david.dorchies@inrae.fr>
2022-02-06 12:43:05 +0100

    fix(set_initial_condition): test and debug
    - add logger for debugging purpose
    - remove extra stuffs in sic project example
    - debug set_initial_condition with successful test
    
    Refs #4

--- R/set_initial_conditions.R
+++ R/set_initial_conditions.R
@@ -3,7 +3,7 @@
 #' Import initial conditions from a scenario/variant result calculation to a new scenario/variant.
 #'
 #' @param params [numeric], arguments passed to Edisic for importing initial conditions. See details
-#' @param cfg
+#' @template param_cfg
 #'
 #' @details `params` [numeric] vector of length 5. Each number correspond to:
 #'
@@ -13,31 +13,32 @@
 #' 1. number of the scenario where the initial conditions are exported to
 #' 1. number of the variant where the initial conditions are exported to  (Use "0" for no variant)
 #'
-#' @return
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' # Import initial conditions from the scenario #1 without variant at time 0s
 #' # to the variant #1 in the scenario #1
-#' set_initial_conditions("1 0 0 1 1")
+#' set_initial_conditions(c(1, 0, 0, 1, 1))
 #' }
 set_initial_conditions <- function(params, cfg = loadConfig()) {
   if (!is.numeric(params) || length(params) != 5) {
     stop("`params` should be a numeric of length 5")
   }
   sArgs <- paste(params, collapse = " ")
-  shell(
-    shQuote(
-      paste(
-        file.path(cfg$sic$path, cfg$sic$edisic),
-        shQuote(dirname(cfg$project$path), type = "cmd"),
-        "CI",
-        shQuote(basename(cfg$project$path), type = "cmd"),
-        sArgs
-      ),
-      type = "cmd2"
+  cmd_line <- shQuote(
+    paste(
+      file.path(cfg$sic$path, cfg$sic$edisic),
+      shQuote(dirname(cfg$project$path), type = "cmd"),
+      "CI",
+      shQuote(basename(cfg$project$path), type = "cmd"),
+      sArgs
     ),
+    type = "cmd2"
+  )
+  logger::log_debug(cmd_line)
+  shell(
+    cmd_line,
     wait = T,
     translate = T
   )
---
+++ tests/testthat/test-set_initial_conditions.R
@@ -0,0 +1,9 @@
+skip_on_ci()
+
+test_that("set_initial_conditions works", {
+  cfg <- loadLocalConfig()
+  sic_run_fortran("fluvia", list(SCE = 1), cfg = cfg)
+  set_initial_conditions(c(1, 0, 0, 1, 1), cfg = cfg)
+  sic_run_fortran("sirene", list(SCE = 1, VAR = 1), cfg = cfg)
+  expect_true(file.exists(gsub("\\.xml", "_1_1.res", cfg$project$path)))
+})
