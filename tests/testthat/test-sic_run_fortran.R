b812a20964b3958d13687f122fd933d95472f16b
Dorchies David <david.dorchies@irstea.fr>
2022-03-11 15:56:59 +0100

    test: run test on Talweg
    Refs #7

--- tests/testthat/test-sic_run_fortran.R
+++ tests/testthat/test-sic_run_fortran.R
@@ -10,6 +10,6 @@ test_that("fluvia on SCE=1 should create a binary result file", {
 
 test_that("talweg should update xml project", {
   mtime_before <- file.mtime(cfg$project$path)
-  sic_run_fortran("talweg", list(INTERF = 0), cfg = cfg)
+  sic_run_fortran("talweg", cfg = cfg)
   expect_gt(file.mtime(cfg$project$path), mtime_before)
 })
