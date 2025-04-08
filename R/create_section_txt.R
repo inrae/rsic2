ac6ba43cb12acaa58ed4241f7cc5c1990ee74ac3
David Dorchies <david.dorchies@inrae.fr>
2025-04-08 22:41:02 +0200

    feat(create_section_txt): add rectangular, circular and dalot section types

--- R/create_section_txt.R
+++ R/create_section_txt.R
@@ -7,44 +7,93 @@
 #' @param distance_majeur [logical] or [numeric], `FALSE` for a minor bed section
 #' @param singular [logical] `TRUE` for a singular section, `FALSE` for a regular section
 #'
+#' @details
+#' The parameter `profile` should respect the formats depending on section
+#' types.
+#' For parametrized section (rectangular, circular, trapezoidal), `profile`
+#'
 #' @return [character], section description in SIC text import format.
 #' @export
 #' @import utils
 #' @examples
 #' # Trapezoidal section
 #' create_section_txt("Trapeze", 0, "T", list(B = 2, S = 1.5, ZF = 100, ZB = 102))
-create_section_txt <- function(section_name,
-                               abscissa,
-                               section_type,
-                               profile,
-                               distance_majeur = FALSE,
-                               singular = FALSE) {
-
-  if (section_type == "T") {
-    if (!is.list(profile) || !all(c("B", "S", "ZF", "ZB") %in% names(profile))) {
-      stop("With `section_type = \"T\", `profile` should be a list with the items B, S, ZF and ZB")
-    }
-    sic_profile <- c("0", paste(profile$B, profile$S, sep = "\t"), paste(profile$ZB, profile$ZF, sep = "\t"))
-  } else if (section_type %in% c("A", "L")) {
+#' # Rectangular section
+#' create_section_txt("Rectangle", 0, "R", list(B = 2, ZF = 100, ZB = 102))
+#' # Circular section
+#' create_section_txt("Circular", 0, "C", list(R = 0.5, ZF = 100, ZB = 102))
+create_section_txt <- function(
+  section_name,
+  abscissa,
+  section_type,
+  profile,
+  distance_majeur = FALSE,
+  singular = FALSE
+) {
+  if (section_type %in% c("A", "L")) {
     if (!is.matrix(profile) || ncol(profile) != 2) {
-      stop("With `section_type = \"A\" or \"L\", `profile` should be a matrix with 2 columns")
+      stop(
+        "With `section_type = \"A\" or \"L\", `profile` should be a matrix with 2 columns"
+      )
     }
-    sic_profile <- capture.output(write.table(profile, sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE))
+    sic_profile <- capture.output(write.table(
+      profile,
+      sep = "\t",
+      quote = FALSE,
+      col.names = FALSE,
+      row.names = FALSE
+    ))
   } else {
-    stop("section_type ", section_type, " not allowed. Possible choices are: A, L and T")
+    # Parametrized sections
+    if (section_type == "R") {
+      check_section_type(section_type, profile, c("B"))
+      sic_profile <- c("0", profile$B)
+    } else if (section_type == "C") {
+      check_section_type(section_type, profile, c("R"))
+      sic_profile <- c("0", profile$R)
+    } else if (section_type %in% c("T", "D")) {
+      check_section_type(section_type, profile, c("B", "S"))
+      sic_profile <- c("0", paste(profile$B, profile$S, sep = "\t"))
+    } else {
+      stop(
+        "section_type ",
+        section_type,
+        " not allowed. Possible choices are: A, L and T"
+      )
+    }
+    sic_profile <- c(sic_profile, paste(profile$ZB, profile$ZF, sep = "\t"))
   }
 
-  if (!(is.logical(distance_majeur) || is.numeric(distance_majeur)) || length(distance_majeur) != 1) {
+  if (
+    !(is.logical(distance_majeur) || is.numeric(distance_majeur)) ||
+      length(distance_majeur) != 1
+  ) {
     stop("`distance_majeur` should be a `logical` or a `numeric` of length 1")
   }
   bMajorBed <- !is.logical(distance_majeur)
-  section_txt <- c(paste(section_name,
-          abscissa,
-          ifelse(bMajorBed, distance_majeur, ""),
-          ifelse(bMajorBed, "1", "0"),
-          paste0(section_type, ifelse(singular, "S", "")),
-          sep = " $ "),
-    sic_profile)
+  section_txt <- c(
+    paste(
+      section_name,
+      abscissa,
+      ifelse(bMajorBed, distance_majeur, ""),
+      ifelse(bMajorBed, "1", "0"),
+      paste0(section_type, ifelse(singular, "S", "")),
+      sep = " $ "
+    ),
+    sic_profile
+  )
   class(section_txt) <- c("SectionTxt", class(section_txt))
   return(section_txt)
+}
+
+check_section_type <- function(section_type, profile, required_params) {
+  required_params <- c(required_params, "ZF", "ZB")
+  if (!is.list(profile) || !all(required_params %in% names(profile))) {
+    stop(
+      "With `section_type = \"",
+      section_type,
+      "\", `profile` should be a list with the items",
+      paste(required_params, collapse = ", ")
+    )
+  }
 }
