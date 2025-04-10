fb603eca6673ef7745e785a3dc83aefcf904b431
David Dorchies <david.dorchies@inrae.fr>
2025-04-10 16:25:26 +0200

    feat: add get_xml_section_num_by_name and get_xml_reach_num_by_node_num

--- R/get_xml_num_by_name.R
+++ R/get_xml_num_by_name.R
@@ -1,10 +1,14 @@
-#' Retrieve node or reach number from XML project file
+#' Retrieve node, reach or reach and section number from XML project file
+#'
+#' @details
+#' In case of several XML nodes are matching the name, the first one is returned.
+#' Providing `cfg` is not necessary if `xp` is provided.
 #'
 #' @param name [character] name of the node or reach
-#' @param xp [xml2::xml_document] the XML project file. Default is the one in the configuration
+#' @param xp A `xml_document` of the XML project file. Default is the one in the configuration
 #' @template param_cfg
 #'
-#' @returns [integer] the number of the node or reach in the XML project file
+#' @returns [integer] the number of the node, reach, or reach and section in the XML project file
 #' @rdname get_xml_num_by_name
 #' @export
 #'
@@ -12,6 +16,7 @@
 #' xp <- xml2::read_xml(system.file("sic_project_test1.xml", package = "rsic2"))
 #' get_xml_node_num_by_name("V1-droite", xp)
 #' get_xml_reach_num_by_name("Bief3", xp)
+#' get_xml_section_num_by_name("dyke", xp)
 get_xml_node_num_by_name <- function(name, xp = xml2::read_xml(cfg$project$path), cfg) {
   # Get the node number
   xPath <- sprintf("/Reseau/Liste_Noeuds/Noeud[@Nom='%s']", name)
@@ -36,7 +41,26 @@ get_xml_reach_num_by_name <- function(name, xp = xml2::read_xml(cfg$project$path
   return(as.integer(node_num))
 }
 
+#' @rdname get_xml_num_by_name
+#' @export
+get_xml_section_num_by_name <- function(name, xp = xml2::read_xml(cfg$project$path), cfg) {
+  # Get the node number
+  xPath <- sprintf("/Reseau/Liste_Biefs/Bief/Liste_Sections/SectionMin[@Nom='%s']", name)
+  node <- xp %>% xml2::xml_find_first(xPath)
+  if (is.na(node)) {
+    stop(sprintf("Section with name '%s' not found in the XML project file.", name))
+  }
+  section_num <- xml2::xml_attr(node, "Num")
+  # Get the reach number
+  reach_num <- node |> xml2::xml_parent() |> xml2::xml_parent() |> xml2::xml_attr("Num")
+  return(as.integer(c(reach_num, section_num)))
+}
+
 #' @title Get reach number from node number
+#'
+#' @details
+#' In case of several XML nodes are matching, the first one is returned.
+#' Providing `cfg` is not necessary if `xp` is provided.
 #'
 #' @param node_num [numeric] node number in the SIC model
 #' @param upstream [logical] if TRUE, get the reach number from the upstream node, otherwise downstream
