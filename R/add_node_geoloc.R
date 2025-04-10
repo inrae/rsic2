6d6598b72397222a3a23ba0233683d2cbdfdb1a6
David Dorchies <david.dorchies@inrae.fr>
2025-04-10 15:31:43 +0200

    feat(add_node_geoloc): add node display relocation from geolocation
    also separate node name update in a separated function

--- R/add_node_geoloc.R
+++ R/add_node_geoloc.R
@@ -1,26 +1,75 @@
 #' Add coordinates on nodes in a SIC project XML file
 #'
 #' @param x,y [numeric], coordinates of the nodes in the SIC project
-#' @param new_name [character] [vecto] of replacement names for the nodes
+#' @param display_xy [data.frame] with 2 rows and 3 columns `num`, `x`, and `y`
 #' @template param_cfg
 #'
 #' @returns Nothing. Use for side effect on the XML project file.
 #' @export
 #'
-add_node_geoloc <- function(x, y, new_names = NULL, cfg) {
+add_node_geoloc <- function(x, y, display_xy = NULL, cfg) {
   xp <- xml2::read_xml(cfg$project$path)
   xPath_template <- "/Reseau/Liste_Noeuds/Noeud[%i]"
+
+  if (!is.null(display_xy)) {
+    scale_x <- (display_xy[2, "x"] - display_xy[1, "x"]) / (x[display_xy[2, "num"]] - x[display_xy[1, "num"]])
+    scale_y <- (display_xy[2, "y"] - display_xy[1, "y"]) / (y[display_xy[2, "num"]] - y[display_xy[1, "num"]])
+    origin <- list(
+      x = list(display = display_xy[1, "x"], geo = x[display_xy[1, "num"]]),
+      y = list(display = display_xy[1, "y"], geo = y[display_xy[1, "num"]]))
+  }
+  if (!is.null(display_xy)) {
+    x_display <- as.integer((x - origin$x$geo) * scale_x + origin$x$display)
+    y_display <- as.integer((y - origin$y$geo) * scale_y + origin$y$display)
+  }
 
   for (i in seq_along(x)) {
     xPath <- sprintf(xPath_template, i)
     node <- xp %>% xml2::xml_find_first(xPath)
-    if (!is.null(new_names)) {
-      xml2::xml_attr(node, "Nom") <- new_names[i]
-    }
     nodeX <- node |> xml2::xml_child("Aff") |> xml2::xml_child("Xgeo")
     xml2::xml_text(nodeX) <- as.character(x[i])
     nodeY <- node |> xml2::xml_child("Aff") |> xml2::xml_child("Ygeo")
     xml2::xml_text(nodeY) <- as.character(y[i])
+    if (!is.null(display_xy)) {
+      nodeX <- node |> xml2::xml_child("Aff") |> xml2::xml_child("X")
+      xml2::xml_text(nodeX) <- as.character(x_display[i])
+      nodeY <- node |> xml2::xml_child("Aff") |> xml2::xml_child("Y")
+      xml2::xml_text(nodeY) <- as.character(y_display[i])
+    }
+  }
+
+  if (!is.null(display_xy)) {
+    # Relocate middle position of the reaches
+    xPath <- "/Reseau/Liste_Biefs/Bief"
+    lapply(
+      xml2::xml_find_all(xp, xPath),
+      function(xml_bief) {
+        # Get the coordinates of the upstream and downstream nodes
+        coords <- calc_middle_reach_display_coords(xp, xml_bief)
+        # Set the coordinates in the XML file
+        nodeX <- xml_bief |> xml2::xml_child("Aff") |> xml2::xml_child("X_Millieu")
+        xml2::xml_text(nodeX) <- as.character(coords[1])
+        nodeY <- xml_bief |> xml2::xml_child("Aff") |> xml2::xml_child("Y_Millieu")
+        xml2::xml_text(nodeY) <- as.character(coords[2])
+      })
   }
   xml2::write_xml(xp, cfg$project$path)
+}
+
+calc_middle_reach_display_coords <- function(xp, xml_bief) {
+  up_node_id <- xml_bief |> xml2::xml_find_first("Top/NoeudAm") |> xml2::xml_text()
+  up_node_coords <- get_node_display_coords(xp, up_node_id)
+  down_node_id <- xml_bief |> xml2::xml_find_first("Top/NoeudAv") |> xml2::xml_text()
+  down_node_coords <- get_node_display_coords(xp, down_node_id)
+  x_display <- (up_node_coords[1] + down_node_coords[1]) / 2
+  y_display <- (up_node_coords[2] + down_node_coords[2]) / 2
+  return(as.integer(c(x_display, y_display)))
+}
+
+get_node_display_coords <- function(xp, node_id) {
+  xPath <- sprintf("/Reseau/Liste_Noeuds/Noeud[@Num=%s]/Aff", node_id)
+  node <- xp %>% xml2::xml_find_first(xPath)
+  x_display <- node |> xml2::xml_child("X") |> xml2::xml_text()
+  y_display <- node |> xml2::xml_child("Y") |> xml2::xml_text()
+  return(as.integer(c(x_display, y_display)))
 }
---
+++ R/update_nodes_names.R
@@ -0,0 +1,32 @@
+#' Update nodes names in a SIC model XML file
+#'
+#' @param new_names [character], new names for the nodes in the SIC model
+#' @template param_cfg
+#'
+#' @returns Nothing. Use for side effect on the XML project file.
+#' @export
+#'
+update_nodes_names <- function(new_names, cfg) {
+  # Check if new_names is a character vector
+  if (!is.character(new_names)) {
+    stop("new_names must be a character vector")
+  }
+
+  # Write the updated XML file
+  xp <- xml2::read_xml(cfg$project$path)
+  xPath_template <- "/Reseau/Liste_Noeuds/Noeud"
+  xml_nodes <- xml2::xml_find_all(xp, xPath_template)
+
+  # Check if new_names has the same length as the number of nodes
+  if (length(new_names) != length(xml_nodes)) {
+    stop("new_names must have the same length as the number of nodes")
+  }
+
+  # Update node names in the XML file
+  for (i in seq_along(new_names)) {
+    xml2::xml_attr(xml_nodes[[i]], "Nom") <- new_names[i]
+  }
+
+  xml2::write_xml(xp, cfg$project$path)
+
+}
